be478745695003efc655437ad79bd564987f54f7
Manish Thulung <manish.thulung58@gmail.com>
2023-06-15 09:06:12 +0545

    next config file bug fixed

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,19 @@
-"use client";
-import { NextPage } from "next";
-import IsAuth from "@/components/auth/IsAuth";
+// "use client";
+// import { NextPage } from "next";
+// import IsAuth from "@/components/auth/IsAuth";
+// import React from "react";
+
+// const AdminPage: NextPage = () => {
+//   return <div>AdminPage</div>;
+// };
+
+// // export default AdminPage;
+// export default IsAuth(AdminPage);
+
 import React from "react";
 
-const AdminPage: NextPage = () => {
-  return <div>AdminPage</div>;
-};
+function page() {
+  return <div>page</div>;
+}
 
-// export default AdminPage;
-export default IsAuth(AdminPage);
+export default page;
